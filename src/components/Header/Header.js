27ff489457bed2d52d0a9994509427b465184e02
George Higbie <gehigbie@gmail.com>
2018-06-15 03:03:48 +0000

    Add content to main content component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+    <div>
+        <h1>Header</h1>
+    </div>
+};
+
+export default Header;
---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MainContentcss';
+
+class MainContent extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Main Content</h1>
+            </div>
+        )
+    }
+}
+
+export default MainContent;
