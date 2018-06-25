69a85ea1d8ab10d9c3950799573c8f70b2ae9265
George Higbie <gehigbie@gmail.com>
2018-06-25 14:51:56 +0000

    Edit data

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -3,7 +3,7 @@ const generalCommands = {
         command: 'mysql-ctl cli',
         description: 'Starts the MYSQL server'
     },
-    show: {
+    showDatabases: {
         command: 'SHOW DATABASES',
         description: 'Provides a list of all databases on the MYSQL server'
     },
@@ -22,6 +22,18 @@ const generalCommands = {
     select: {
         command: 'SELECT database()',
         description: 'Shows the database that is currently in use'
+    },
+    showTables: {
+        command: 'SHOW TABLES',
+        description: 'Shows a list of tables'
+    },
+    showColumns: {
+        command: 'SHOW COLUMNS FROM <table name>',
+        description: 'Shows the contents of a table'
+    },
+    describe: {
+        command: 'DESC <table name>',
+        description: 'Shows the contents of a table'
     }
 };
 
