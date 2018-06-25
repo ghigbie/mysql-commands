55ee01aedd63b14bca1e847b5f70c1618d00f9fa
George Higbie <gehigbie@gmail.com>
2018-06-25 16:44:22 +0000

    Change data structure

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,46 +1,53 @@
-const generalCommands = {
-    start: {
+const generalCommands = [
+    {
         command: 'mysql-ctl cli',
         description: 'Starts the MYSQL server'
     },
-    showDatabases: {
+    {
         command: 'SHOW DATABASES',
         description: 'Provides a list of all databases on the MYSQL server'
     },
-    create: {
+    {
         command: 'CREATE DATABASE <database name>',
         description: 'Creates a database for use'
     },
-    drop: {
+    {
         command: 'DROP DATABASE <database name>',
         description: 'Deletes a database, so that it can no longer be used'
     },
-    use: {
+    {
         command: 'USE <database name>',
         description: 'Selects a database to use'
     },
-    select: {
+    {
         command: 'SELECT database()',
         description: 'Shows the database that is currently in use'
     },
-    showTables: {
+    {
         command: 'SHOW TABLES',
         description: 'Shows a list of tables'
     },
-    showColumns: {
+    {
         command: 'SHOW COLUMNS FROM <table name>',
         description: 'Shows the contents of a table'
     },
-    describe: {
+    {
         command: 'DESC <table name>',
         description: 'Shows the contents of a table'
     },
-    dropTable: {
-        command: 'DROP <table name>',
+    {
+        command: 'DROP TABLE <table name>',
         description: 'Deletes a table from the database'
     }
-};
+];
 
 const insertingData = {
-    
-}
+    insert: {
+        command: 'INSERT INTO <table name> (<column name>, <column name>) VALUES (<actual value>, <actual value>)',
+        description: 'Inserts data into table'
+    },
+    view: {
+        command: 'SELECT * FROM <table name>',
+        description: 'Shows all data in a table'
+    }
+};
